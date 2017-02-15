7b5cfadd45f4d0e6c1baea1270355e8623672836
Peter Haight <peterh@giantrabbit.com>
2017-02-15 18:15:39 +0000

    Print out some stuff on clear cookie page
    It's not really clear it's working unless you print out some stuff.
    
    Issue #29071

--- clear.php
+++ clear.php
@@ -3,3 +3,12 @@
 foreach ($_COOKIE as $key => $value) {
   setcookie($key, 'deleted', 1, '/', '.thecmg.info');
 }
+?>
+<html>
+  <head>
+    <title>Cookies Cleared</title>
+  </head>
+  <body>
+    Cookies cleared.
+  </body>
+</html>
